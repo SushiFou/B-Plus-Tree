11f8b76f2ec64daeaa27897c4969c89dc91dee43
gouzmi <guillaume.dupont.rm@gmail.com>
2020-11-03 19:08:28 +0100

    delete csv

--- Generator_Data.py
+++ Generator_Data.py
@@ -1,7 +1,7 @@
 import random
 random.seed(702140)
 
-n=15
+n=20
 
 List_Nicknames=["Assi","Aggy","Ben","Bob","Bully" ,"Cobra","Cats Eyes" ,"Cloudy",
 "Dolfo" ,"Dutch","Eagle", "Fish","Fighter","Gabby" ,"Hoppy" ,"Hamish" ,"Igo" ,
